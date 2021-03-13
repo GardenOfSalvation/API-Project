a92f7b31895d7242f92517e6767b630b3a066f9b
Suejin Kim <kim.suejin.20@gmail.com>
2021-03-13 11:00:17 -0500

    adding javascript for fetching api data

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,38 @@
 // giphy key : aWmCF38aWZNhbJoi236yO2yuW4smGy9q
 // url "https://api.giphy.com/v1/" 
 
+//Suejin's Javascript for the two apis:
+var ApiKey = "774092c9-0b11-4331-9432-a0aac1f1ca4a"
+var link = "https://api.thedogapi.com/v1/images/search" + "?api_key=" + ApiKey;
+var containerEl = $("#container")
+var link2 = "https://namey.muffinlabs.com/name.json?frequency=all"
+
+fetch (link)
+    .then(function(response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        var url = document.createElement("img");
+        url.src = data[0].url;
+        url.width= "200"
+        containerEl.append(url);
+    })
+
+
+fetch(link2)
+    .then(function(response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        var names = document.createElement("h2");
+        names.textContent = data[0];
+        containerEl.append(names);
+    })
+
+    .catch(err => {
+        console.error(err);
+    });
